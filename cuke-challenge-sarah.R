e2f51d419ab705e8e23d0231779970a2fb369f7d
Sarah Carey <sarah.carey@ufl.edu>
2015-02-18 13:16:22 -0500

    add response

--- cuke-challenge-sarah.R
+++ cuke-challenge-sarah.R
@@ -36,5 +36,31 @@ hol_class_noFalse
 hol_class_noFalse_count <- nzchar(hol_class_noFalse)
 sum(hol_class_noFalse_count == "FALSE") ## 0
 
-#5 (to be completed soon)
-#6 (to be completed soon)
+#5 
+nom_subgenus <- nom[c("Genus.current", "Subgenus.current")]
+nom_sub <- tapply(nom_subgenus$Subgenus.current, nom_subgenus$Genus.current, unique)
+nom_sub
+## This is most likely not an ideal way of doing this, but it did work. The answer is Holothuria.
+
+#6
+genus_species_hol <- paste(hol$dwc.genus, hol$dwc.specificEpithet)
+head(genus_species_hol) #to check if worked
+hol <- cbind(hol, genusspecies=genus_species_hol)
+head(hol)
+
+genus_species_nom <- paste(nom$Genus.current, nom$species.current)
+head(genus_species_nom)
+nom <- cbind(nom, genusspecies=genus_species_nom)
+head(nom)
+
+hol_nom <- merge(hol, nom, all.x = TRUE)
+
+hol_invalid  <- subset(hol_nom, is.na(hol_nom$Status))
+hol_invalid_onlyColumns <- hol_invalid [c("idigbio.uuid",
+                                                "dwc.genus",
+                                                "dwc.specificEpithet",
+                                                "dwc.institutionCode",
+                                                "dwc.catalogNumber")]
+
+write.csv(hol_invalid_onlyColumns, "data/holothuriidae-invalid.csv")
+
